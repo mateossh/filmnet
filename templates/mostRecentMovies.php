3468efe166e31c2ebadc4d099697b6ea63549782
Mateusz Ż <thedead4fun@live.com>
2017-04-18 13:43:22 +0200

    Update templates rendered on /index
    Now if you click on any title/image, you will be redirected to specific
    movie

--- templates/mostRecentMovies.php
+++ templates/mostRecentMovies.php
@@ -2,10 +2,10 @@
 	<h2 class="List__Title">Premiery</h2>
 	<ul class="Carousel">
 		<?php foreach($this->get("mostRecent") as $movie): ?>
-		<li class="List__Item">
-			<img src="" />
-			<h3><?= $movie["title"] ?></h3>
-		</li>
+			<li class="List__Item"><a href="index.php?action=singleMovie&task=<?= $movie['id'] ?>">
+				<img src="<?= $movie['poster'] ?>" />
+				<h3><?= $movie['title'] ?></h3>
+			</a></li>
 		<?php endforeach; ?>
 	</ul>
 </section>
