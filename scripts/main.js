e3381e12e7fbe6e03cf2b1bf3335fa7ad0232c3c
Mateusz Ż <thedead4fun@live.com>
2017-04-21 15:49:25 +0200

    Add autoplay on desktops

--- scripts/main.js
+++ scripts/main.js
@@ -34,13 +34,15 @@ $(document).ready(function() {
 			{
 				breakpoint: 1024,
 				settings: {
-					slidesToShow: 5
+					slidesToShow: 5,
+					autoplay: true
 				}
 			},
 			{
 				breakpoint: 900,
 				settings: {
-					slidesToShow: 4
+					slidesToShow: 4,
+					autoplay: true
 				}
 			},
 			{
